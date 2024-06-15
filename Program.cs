e2273e05ed083ab6152810030bccf1c9cebb4721
Kamil Nowak <kamil.nowak@mail.com>
2024-06-15 13:26:00 +0200

    init

---
+++ Program.cs
@@ -0,0 +1,16 @@
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+app.UseSwagger();
+
+app.UseSwaggerUI(c =>
+{
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "Hacker News V1");
+});
+app.UseHttpsRedirection();
+
+app.Run();
